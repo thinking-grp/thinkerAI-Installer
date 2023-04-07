3999c35026c7619a6d00b8736182b02f211e356e
amania <91243704+amania-Jailbreak@users.noreply.github.com>
2023-04-07 15:43:02 +0900

    🐛 darwin向けスクリプトの修正

--- build-darwin.sh
+++ build-darwin.sh
@@ -1,1 +1,1 @@
-python -m PyInstaller --add-data ./lib/window.ui ./lib --add-data ./assets ./assets main.py --noconsole --onefile
+python3 -m PyInstaller --add-data ./lib/window.ui:./lib/window.ui --add-data ./assets:./assets main.py --noconsole --onefile
